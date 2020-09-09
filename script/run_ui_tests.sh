9d0b6f8f49eadea9d6284f69d35b6431da8e15e1
Santiago Baldassin <sbaldassin@gmail.com>
2020-09-09 16:17:39 -0300

    building

--- script/run_ui_tests.sh
+++ script/run_ui_tests.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-docker run -p 3000:3000 -d metabase/metabase
-
-sleep 20
-echo "Metabase launched"
-
-#docker run -it -v $PWD/:/e2e --entrypoint=cypress -w /e2e cypress/included:3.2.0 run --spec cypress/integration/metabase-ui-test.js --config baseUrl=http://host.docker.internal:3000,pageLoadTimeout=100000,defaultCommandTimeout=10000
-
-npx cypress run --spec "cypress/integration/metabase-ui-test.js"
+ #!/bin/bash
+ docker-compose -f docker-compose-e2e.yml up --exit-code-from cypress

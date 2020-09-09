dcf468eb31558db140cb73c5cdfbc17d71df876a
Santiago Baldassin <sbaldassin@gmail.com>
2020-09-09 14:41:06 -0300

    bulding

---
+++ script/run_api_tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker run -p 3000:3000 -d metabase/metabase
+sleep 30
+docker run -it -v $PWD/:/e2e --entrypoint=cypress -w /e2e cypress/included:3.2.0 run --spec cypress/integration/metabase-api-test.js --config baseUrl=http://host.docker.internal:3000
--- script/run_ui_tests.sh
+++ script/run_ui_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker run -p 3000:3000 -d metabase/metabase
 sleep 30
-npx cypress run --spec "cypress/integration/metabase-ui-test.js"
+docker run -it -v $PWD/:/e2e --entrypoint=cypress -w /e2e cypress/included:3.2.0 run --spec cypress/integration/metabase-ui-test.js --config baseUrl=http://host.docker.internal:3000
